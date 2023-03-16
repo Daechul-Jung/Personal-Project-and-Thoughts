ae5f3e7d2c04b9db05fd6cbe87614430b26b0b7e
Daechul <jungdaechul@berkeley.edu>
2023-03-15 17:07:37 -0700

    libraries

--- Starting/main.py
+++ Starting/main.py
@@ -1,7 +1,27 @@
-print('Hello World')
+import matplotlib as matplotlib
+import numpy as np
 
-def calculating (x,y,z):
-    return x+y+z
+import pandas as pd
+# %matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import OneHotEncoder
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import zipfile
+import os
+from sklearn.preprocessing import OneHotEncoder
 
 
-print(calculating(1,2,3))
+
+
+def calculating(x, y, z):
+    return x + y + z
+
+
+print(calculating(1, 2, 3))
+
+
